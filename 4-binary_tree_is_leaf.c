4db3fa62912890c11d7cfc6aee72a73c38d6ddfa
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-07 19:23:40 -0600

    binary tree is leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,10 +10,10 @@
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
-		return (NULL);
+		return (0);
 
 	if (node->left == NULL && node->right == NULL)
-		return (NULL);
+		return (1);
 
 	return (0);
 }

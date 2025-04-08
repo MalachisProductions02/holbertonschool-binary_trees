0def5e7769bd79c6ef88bc13c218ac712e6b6f68
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-07 20:12:30 -0600

    binary tree is root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -7,7 +7,7 @@
  * @node: Pointer to the node to check
  * Return: 1 if node is a root, 0 otherwise.
  */
-int binary_tree:is_root(const binary_tree_t *node)
+int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);

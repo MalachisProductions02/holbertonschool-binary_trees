17462dc72e82e56a62aba65e9fc189a84d1edec1
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-08 17:51:03 -0600

    binary tree balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,11 +9,11 @@
  */
 static int height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-
 	int left_height = height(tree->left);
 	int right_height = height(tree->right);
+	
+	if (tree == NULL)
+		return (0);
 
 	return (1 + (left_height > right_height ? left_height : right_height));
 }

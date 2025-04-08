be43bf6eed4451354c260dd2f5f4b84c1b0e03dc
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-08 11:47:47 -0600

    binary tree height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,7 +15,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	left_h = binary_tree_height(tree->left);
-	right_h- = binary_tree_height(tree->right);
+	right_h = binary_tree_height(tree->right);
 
 	return ((left_h > right_h ? left_h : right_h) + 1);
 }

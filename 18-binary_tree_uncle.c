a3cb79f76b898146a7b4f38ad3d522a0e6581c8b
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-08 22:19:03 -0600

    binary tree uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -16,5 +16,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node->parent == node->parent->parent->left)
 		return (node->parent->parent->right);
 	else
-		return (node->parent->parent>left);
+		return (node->parent->parent->left);
 }

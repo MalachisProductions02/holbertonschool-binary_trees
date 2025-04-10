5cb467d8502fa0052527c2c397b60b2203f0d082
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-04-10 21:57:35 +0000

    binary tree is complete

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,62 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * queue_push - Adds a node to the end of the queue
+ * @queue: Double pointer to the queue
+ * @node: Node to add
+ */
+void queue_push(binary_tree_t ** queue, binary_tree_t *node, size_t *size, size_t *capacity)
+{
+	if (*size >= *capacity)
+	{
+		*capacity += 10;
+		*queue = realloc(*queue, *capacity * sizeof(binary_tree_t *));
+	}
+	(*queue)[(*size)++] = node;
+}
+
+/**
+ * binary_tree_is_complete - Checks if a binary tree is complete
+ * @tree: Pointer to the root node of the tree
+ * Return: 1 if complete, 0 otherwise
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	binary_tree_t **queue = NULL;
+	size_t size = 0, capacity = 0, i = 0;
+	int seen_null = 0;
+
+	if (!tree)
+		return (0);
+
+	queue = malloc(10 * sizeof(binary_tree_t));
+	if (!queue)
+		return (0);
+	capacity = 10;
+
+	queue_push(&queue, (binary_tree_t *)tree, &size, &capacity);
+
+	while (i < size)
+	{
+		binary_tree_t *node = queue[i++];
+
+		if (!node)
+		{
+			seen_null = 1;
+		}
+		else
+		{
+			if (seen_null)
+			{
+				free(queue);
+				return (0);
+			}
+			queue_push(&queue, node->left, &size, &capacity);
+			queue_push(&queue, node->right, &size, &capacity);
+		}
+	}
+
+	free(queue);
+	return (1);
+}

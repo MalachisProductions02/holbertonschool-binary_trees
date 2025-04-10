6abb85308316351ee2e0687f7bd439cfce7700c2
MalachisProductions02 <malachisproductionsnooficial@gmail.com>
2025-04-10 19:42:57 +0000

    main

--- binary_trees.h
+++ binary_trees.h
@@ -34,6 +34,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_print(const binary_tree_t *);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);

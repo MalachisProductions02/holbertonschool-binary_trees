5c86955adf764abf26560f74ba5902f36060d69c
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-11 13:24:09 -0600

    array to bst

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -14,7 +14,7 @@ bst_t *array_to_bst(int *array, size_t size)
 	if (array == NULL || size == 0)
 		return (NULL);
 
-	for (i = 0, i < size, i++)
+	for (i = 0; i < size; i++)
 		bst_insert(&root, array[i]);
 
 	return (root);

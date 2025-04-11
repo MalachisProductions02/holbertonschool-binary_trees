2aaab6a8f7f2c46bfa0d1791a8a3dd22a614e863
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-11 13:12:01 -0600

    bst insert

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,40 +1,55 @@
 #include "binary_trees.h"
 
 /**
- * bst_insert - Inserts a value into  a Binary Search Tree
+ * bst_insert - Inserts a value into a Binary Search Tree
  * @tree: Double pointer to the root node of the BST
  * @value: Value to insert
- * Return: Pointer to the newly created node, or NULL on failure
+ *
+ * Return: Pointer to the created node, or NULL on failure
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
+	bst_t *current, *new_node;
+
 	if (tree == NULL)
 		return (NULL);
-	
+
 	if (*tree == NULL)
 	{
 		*tree = binary_tree_node(NULL, value);
 		return (*tree);
 	}
 
-	if (value < (*tree)->n)
+	current = *tree;
+	while (current != NULL)
 	{
-		if ((*tree)->left)
-			return (bst_insert(&(*tree)->left, value));
-		else
+		if (value < current->n)
 		{
-			(*tree)->left = binary_tree_node(*tree, value);
-			return ((*tree)->left);
+			if (current->left == NULL)
+			{
+				new_node = binary_tree_node(current, value);
+				if (new_node == NULL)
+					return (NULL);
+				current->left = new_node;
+				return (new_node);
+			}
+			current = current->left;
 		}
-	}
-	else if (value > (*tree)->n)
-	{
-		if ((*tree)->right)
-			return (bst_insert(&(*tree)->right, value));
+		else if (value > current->n)
+		{
+			if (current->right == NULL)
+			{
+				new_node = binary_tree_node(current, value);
+				if (new_node == NULL)
+					return (NULL);
+				current->right = new_node;
+				return (new_node);
+			}
+			current = current->right;
+		}
 		else
 		{
-			(*tree)->right = binary_tree_node(*tree, value);
-			return ((*tree)->right);
+			return (NULL);
 		}
 	}
 

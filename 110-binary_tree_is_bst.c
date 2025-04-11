18591d8384bf8f7a460b083583118f4312a43d1a
Malachis Productions <malachisproductionsnooficial@gmail.com>
2025-04-11 12:24:29 -0600

    binary tree is bst

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,34 +1,34 @@
 #include "binary_trees.h"
-#include <stdlib.h>
 #include <limits.h>
 
 /**
- * is_bst_helper - Helper to validate BST by checking node
- * values in range
- * @node: Pointer to the current node
- * @min: Minimum allowed value for the current node
- * @max: Maximum allowed value for the current node
- * Return: 1 if subtreeis valid BST, 0 otherwise
+ * is_bst_helper - Helper to check if tree is a valid BST within min/max range
+ * @node: Pointer to the node to check
+ * @min: Minimum value allowed
+ * @max: Maximum value allowed
+ * Return: 1 if valid BST, 0 otherwise
  */
 int is_bst_helper(const binary_tree_t *node, int min, int max)
 {
-	if (!node)
-		return (1);
+    if (!node)
+        return (1);
 
-	if (node->n <= min || node->n >= max)
-		return (0);
-	return (is_bst_helper(node->left, min, node->n) &&
-			is_bsthelper(node->right, node->n, max));
+    if (node->n <= min || node->n >= max)
+        return (0);
+
+    return (is_bst_helper(node->left, min, node->n) &&
+            is_bst_helper(node->right, node->n, max));
 }
 
 /**
  * binary_tree_is_bst - Checks if a binary tree is a valid BST
- * @tree: Pointer to the root node of the tree to check
- * Return: 1 if valid BST, 0 otherwise
+ * @tree: Pointer to the root node of the tree
+ * Return: 1 if BST, 0 otherwise
  */
-int binary_tree_bst(const binary_tree_t *tree)
+int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+    if (!tree)
+        return (0);
 
-	return (is_bst_helper(tree, INT_MIN, INT_MAX));
+    return (is_bst_helper(tree, INT_MIN, INT_MAX));
+}
